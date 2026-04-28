2e4ce37760f73a32bf0aea9eb57a2c7ce3fef56e
luissantos-oficina <154244440+luissantos-oficina@users.noreply.github.com>
2026-04-28 07:46:21 +0000

    criação de classe viatura

---
+++ gestao_equipas/src/Viatura.java
@@ -0,0 +1,44 @@
+public class Viatura {
+
+    private String tipoCarro;
+    private int numRodas;
+    private int velMaxima;
+
+    
+    public Viatura() {
+    }
+
+    
+    public Viatura(String tipoCarro, int numRodas, int velMaxima) {
+        this.tipoCarro = tipoCarro;
+        this.numRodas = numRodas;
+        this.velMaxima = velMaxima;
+    }
+
+
+    public String getTipoCarro() {
+        return tipoCarro;
+    }
+    public void setTipoCarro(String tipoCarro) {
+        this.tipoCarro = tipoCarro;
+    }
+    public int getNumRodas() {
+        return numRodas;
+    }
+    public void setNumRodas(int numRodas) {
+        this.numRodas = numRodas;
+    }
+    public int getVelMaxima() {
+        return velMaxima;
+    }
+    public void setVelMaxima(int velMaxima) {
+        this.velMaxima = velMaxima;
+    }
+
+    public void mostraInfo() {
+        System.out.println("Vou mostrar a informação do carro");
+        System.out.println("Tipo de Carro: " + this.tipoCarro);
+        System.out.println("Número de Rodas: " + this.numRodas);
+        System.out.println("Velocidade Máxima: " + this.velMaxima);
+    }
+}
